64cc0e604c95c046a256eed15634aed88e454ace
Jose Manuel Mendoza Murillo <jose.manuel.mendoza.murillo@gmail.com>
2024-06-30 15:29:44 -0600

    SAE-2 - Creacion del proyecto spring

---
+++ src/main/java/com/ventuit/adminstrativeapp/AdminstrativeappApplication.java
@@ -0,0 +1,13 @@
+package com.ventuit.adminstrativeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminstrativeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminstrativeappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ventuit/adminstrativeapp/AdminstrativeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ventuit.adminstrativeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminstrativeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
