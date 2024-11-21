4cf67877a7e2386b789960593d1b327d49fa846e
Jose Manuel Mendoza Murillo <jose.manuel.mendoza.murillo@gmail.com>
2024-11-21 03:32:58 +0000

    SAE-38 create direction model

---
+++ src/main/java/com/ventuit/adminstrativeapp/shared/models/DirectionsModel.java
@@ -0,0 +1,53 @@
+package com.ventuit.adminstrativeapp.shared.models;
+
+import com.ventuit.adminstrativeapp.core.models.ExtendedBaseModel;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "directions")
+public class DirectionsModel extends ExtendedBaseModel {
+
+    @Column(length = 60, nullable = false)
+    private String latitude;
+
+    @Column(length = 60, nullable = false)
+    private String longitude;
+
+    @Column(length = 60, nullable = false)
+    private String street;
+
+    @Column(length = 10, nullable = false)
+    private String exteriorNumber;
+
+    @Column(length = 10)
+    private String interiorNumber;
+
+    @Column(length = 60)
+    private String neighborhood;
+
+    @Column(length = 60, nullable = false)
+    private String postalCode;
+
+    @Column(length = 60, nullable = false)
+    private String city;
+
+    @Column(length = 60, nullable = false)
+    private String state;
+
+    @Column(length = 60, nullable = false)
+    private String country;
+
+}
