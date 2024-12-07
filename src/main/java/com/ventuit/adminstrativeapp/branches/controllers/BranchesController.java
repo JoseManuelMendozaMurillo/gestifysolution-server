b27c33face9a337a13811c8fda79040217ec23fd
Jose Manuel Mendoza Murillo <jose.manuel.mendoza.murillo@gmail.com>
2024-12-07 02:43:03 +0000

    SAE-35 Create branches controller on spring boot

---
+++ src/main/java/com/ventuit/adminstrativeapp/branches/controllers/BranchesController.java
@@ -0,0 +1,18 @@
+package com.ventuit.adminstrativeapp.branches.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ventuit.adminstrativeapp.branches.dto.BranchesDto;
+import com.ventuit.adminstrativeapp.branches.services.BranchesService;
+import com.ventuit.adminstrativeapp.core.controllers.implementations.CrudControllerImpl;
+
+@RestController
+@RequestMapping("branches")
+public class BranchesController extends CrudControllerImpl<BranchesDto, Integer, BranchesService> {
+
+    public BranchesController(BranchesService crudService) {
+        super(crudService);
+    }
+
+}
