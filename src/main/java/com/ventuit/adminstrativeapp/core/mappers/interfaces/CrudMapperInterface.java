8af2107afe0de7a9b0bfca391a522636d275c81a
Jose Manuel Mendoza Murillo <jose.manuel.mendoza.murillo@gmail.com>
2024-12-19 00:38:57 +0000

    SAE-54  Creating mapper for warehouses category model

--- src/main/java/com/ventuit/adminstrativeapp/core/mappers/interfaces/CrudMapperInterface.java
+++ src/main/java/com/ventuit/adminstrativeapp/core/mappers/interfaces/CrudMapperInterface.java
@@ -2,7 +2,9 @@ package com.ventuit.adminstrativeapp.core.mappers.interfaces;
 
 import java.util.List;
 
+import org.mapstruct.IterableMapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 import com.ventuit.adminstrativeapp.core.dto.BaseDto;
 import com.ventuit.adminstrativeapp.core.models.BaseModel;
@@ -11,13 +13,17 @@ public interface CrudMapperInterface<CREATINGDTO extends BaseDto, UPDATINGDTO ex
 
     ENTITY toEntity(CREATINGDTO dto);
 
+    @Named("toDto")
     CREATINGDTO toDto(ENTITY entity);
 
+    @IterableMapping(qualifiedByName = "toDto")
     List<CREATINGDTO> entitiesToDtos(List<ENTITY> listEntities);
 
-    List<LISTDTO> entitiesToShowDtos(List<ENTITY> listEntities);
-
+    @Named("toShowDto")
     LISTDTO toShowDto(ENTITY entity);
+
+    @IterableMapping(qualifiedByName = "toShowDto")
+    List<LISTDTO> entitiesToShowDtos(List<ENTITY> listEntities);
 
     ENTITY updateFromDto(UPDATINGDTO dto, @MappingTarget ENTITY entity);
 }
---
+++ src/main/java/com/ventuit/adminstrativeapp/warehouses/mappers/WarehousesCategoriesMapper.java
@@ -0,0 +1,39 @@
+package com.ventuit.adminstrativeapp.warehouses.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants.ComponentModel;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import com.ventuit.adminstrativeapp.core.mappers.interfaces.CrudMapperInterface;
+import com.ventuit.adminstrativeapp.warehouses.dto.CreateWarehousesCategoriesDto;
+import com.ventuit.adminstrativeapp.warehouses.dto.UpdateWarehousesCategoriesDto;
+import com.ventuit.adminstrativeapp.warehouses.models.WarehousesCategoriesModel;
+
+@Mapper(componentModel = ComponentModel.SPRING, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public abstract class WarehousesCategoriesMapper implements
+        CrudMapperInterface<CreateWarehousesCategoriesDto, UpdateWarehousesCategoriesDto, CreateWarehousesCategoriesDto, WarehousesCategoriesModel> {
+
+    @Override
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    @Mapping(target = "deletedBy", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    public abstract WarehousesCategoriesModel toEntity(CreateWarehousesCategoriesDto dto);
+
+    @Override
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "deletedAt", ignore = true)
+    @Mapping(target = "deletedBy", ignore = true)
+    @Mapping(target = "updatedBy", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    public abstract WarehousesCategoriesModel updateFromDto(UpdateWarehousesCategoriesDto dto,
+            @MappingTarget WarehousesCategoriesModel entity);
+
+}
