a17a417c8a938d6ce0dcef8d1583767f9986df7f
Jose Manuel Mendoza Murillo <jose.manuel.mendoza.murillo@gmail.com>
2024-12-19 00:00:13 +0000

    SAE-52  Creating model to warehouses category

---
+++ src/main/java/com/ventuit/adminstrativeapp/warehouses/models/WarehousesCategories.java
@@ -0,0 +1,27 @@
+package com.ventuit.adminstrativeapp.warehouses.models;
+
+import com.ventuit.adminstrativeapp.core.models.ExtendedBaseModel;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "warehouses_categories")
+public class WarehousesCategories extends ExtendedBaseModel {
+    @Column(nullable = false, unique = true, length = 60)
+    private String name;
+
+    @Column(nullable = true, length = 100)
+    private String description;
+}
