85639cad37ab0072fba40135d61148ca40810a8e
Jose Manuel Mendoza Murillo <jose.manuel.mendoza.murillo@gmail.com>
2024-12-22 03:21:06 +0000

    GS-62 Create supplies measure dtos

--- src/main/java/com/ventuit/adminstrativeapp/supplies/models/SuppliesMeasureModel.java
+++ src/main/java/com/ventuit/adminstrativeapp/supplies/models/SuppliesMeasureModel.java
@@ -2,6 +2,7 @@ package com.ventuit.adminstrativeapp.supplies.models;
 
 import com.ventuit.adminstrativeapp.core.models.ExtendedBaseModel;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -19,6 +20,7 @@ import lombok.experimental.SuperBuilder;
 @Table(name = "supplies_measure")
 public class SuppliesMeasureModel extends ExtendedBaseModel {
 
+    @Column(nullable = false, unique = true, length = 60)
     private String measure;
 
 }
