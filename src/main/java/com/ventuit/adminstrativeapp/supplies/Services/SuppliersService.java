121d9d4140b05bd3957018c16ad90d3e87ff9f9a
Jose Manuel Mendoza Murillo <jose.manuel.mendoza.murillo@gmail.com>
2024-12-27 03:33:09 +0000

    GS-78  Create suppliers service

---
+++ src/main/java/com/ventuit/adminstrativeapp/supplies/Services/SuppliersService.java
@@ -0,0 +1,105 @@
+package com.ventuit.adminstrativeapp.supplies.Services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.ventuit.adminstrativeapp.core.services.implementations.CrudServiceImpl;
+import com.ventuit.adminstrativeapp.shared.dto.DirectionsDto;
+import com.ventuit.adminstrativeapp.shared.mappers.DirectionsMapper;
+import com.ventuit.adminstrativeapp.shared.models.DirectionsModel;
+import com.ventuit.adminstrativeapp.shared.repositories.DirectionsRepository;
+import com.ventuit.adminstrativeapp.supplies.dto.CreateSuppliersDto;
+import com.ventuit.adminstrativeapp.supplies.dto.UpdateSuppliersDto;
+import com.ventuit.adminstrativeapp.supplies.mappers.SuppliersMapper;
+import com.ventuit.adminstrativeapp.supplies.models.SuppliersModel;
+import com.ventuit.adminstrativeapp.supplies.repositories.SuppliersRepository;
+
+@Service
+public class SuppliersService extends
+        CrudServiceImpl<CreateSuppliersDto, UpdateSuppliersDto, CreateSuppliersDto, SuppliersModel, Integer, SuppliersMapper, SuppliersRepository> {
+
+    public SuppliersService(SuppliersMapper mapper, SuppliersRepository repository) {
+        super(repository, mapper);
+    }
+
+    @Autowired
+    DirectionsRepository directionsRepository;
+    @Autowired
+    DirectionsMapper directionsMapper;
+
+    @Override
+    public ResponseEntity<?> create(CreateSuppliersDto dto) {
+        if (dto.getDirection() != null) {
+            DirectionsModel direction = this.directionsMapper.toEntity(dto.getDirection());
+            DirectionsModel directionSaved = this.directionsRepository.save(direction);
+            DirectionsDto directionDto = this.directionsMapper.toDto(directionSaved);
+            dto.setDirection(directionDto);
+        }
+        return super.create(dto);
+    }
+
+    @Override
+    public Boolean update(Integer id, UpdateSuppliersDto dto) {
+        Optional<SuppliersModel> optionalSupplier = this.repository.findById(id);
+
+        if (!optionalSupplier.isPresent())
+            return false;
+
+        SuppliersModel supplier = optionalSupplier.get();
+
+        if (dto.getDirection() != null) {
+
+            DirectionsModel direction = supplier.getDirection();
+
+            dto.getDirection().setDeletedAt(direction.getDeletedAt());
+
+            DirectionsModel directionUpdated = this.directionsMapper.updateFromDto(dto.getDirection(), direction);
+
+            if (directionUpdated == null)
+                return false;
+        }
+
+        dto.setDeletedAt(supplier.getDeletedAt());
+
+        SuppliersModel branchUpdated = this.mapper.updateFromDto(dto, supplier);
+
+        return branchUpdated != null;
+    }
+
+    @Override
+    public Boolean softDeleteById(Integer id) {
+        Optional<SuppliersModel> optionalSupplier = this.repository.findById(id);
+
+        if (!optionalSupplier.isPresent())
+            return false;
+
+        SuppliersModel supplier = optionalSupplier.get();
+
+        if (supplier.getDirection() != null) {
+            Integer directionId = supplier.getDirection().getId();
+            this.directionsRepository.softDeleteById(directionId);
+        }
+
+        return super.softDeleteById(id);
+    }
+
+    @Override
+    public Boolean restoreById(Integer id) {
+        Optional<SuppliersModel> optionalSupplier = this.repository.findById(id);
+
+        if (!optionalSupplier.isPresent())
+            return false;
+
+        SuppliersModel supplier = optionalSupplier.get();
+
+        if (supplier.getDirection() != null) {
+            Integer directionId = supplier.getDirection().getId();
+            this.directionsRepository.restoreById(directionId);
+        }
+
+        return super.restoreById(id);
+    }
+}
