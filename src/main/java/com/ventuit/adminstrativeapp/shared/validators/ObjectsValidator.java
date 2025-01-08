32020d2e927d6948c8b404669129fd99cd97a237
Jose Manuel Mendoza Murillo <jose.manuel.mendoza.murillo@gmail.com>
2025-01-08 05:13:03 +0000

    GS-102 Create an object validator

---
+++ src/main/java/com/ventuit/adminstrativeapp/shared/validators/ObjectsValidator.java
@@ -0,0 +1,44 @@
+package com.ventuit.adminstrativeapp.shared.validators;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ValidatorFactory;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+
+@Component
+public class ObjectsValidator<T> {
+
+    private final ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+    private final Validator validator = validatorFactory.getValidator();
+
+    public void validate(T classToValidate) {
+        // Method for validating fields from a class
+
+        Set<ConstraintViolation<T>> violations = this.validator.validate(classToValidate);
+
+        // if (!violations.isEmpty()) {
+        // throw ObjectNotValidException
+        // .builder()
+        // .errorMessage("There are some errors in the request's fields")
+        // .fieldsError(getFieldsErrorMap(violations))
+        // .build();
+        // }
+
+    }
+
+    private HashMap<String, String> getFieldsErrorMap(Set<ConstraintViolation<T>> violations) {
+        HashMap<String, String> fieldsError = new HashMap<>();
+        for (ConstraintViolation<T> violation : violations) {
+            String field = violation.getPropertyPath().toString();
+            String errorMessage = violation.getMessage();
+            fieldsError.put(field, errorMessage);
+        }
+        return fieldsError;
+    }
+
+}
