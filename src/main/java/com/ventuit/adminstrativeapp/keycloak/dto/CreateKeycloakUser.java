816d79dee0b1bf5482074524572c2cf29a0491b5
Jose Manuel Mendoza Murillo <jose.manuel.mendoza.murillo@gmail.com>
2025-01-25 00:00:43 +0000

    GS-112 create keycloak users

--- src/main/java/com/ventuit/adminstrativeapp/keycloak/dto/CreateKeycloakUser.java
+++ src/main/java/com/ventuit/adminstrativeapp/keycloak/dto/CreateKeycloakUser.java
@@ -3,8 +3,11 @@ package com.ventuit.adminstrativeapp.keycloak.dto;
 import com.ventuit.adminstrativeapp.keycloak.validations.uniqueemail.UniqueEmail;
 import com.ventuit.adminstrativeapp.keycloak.validations.uniqueusername.UniqueUsername;
 import com.ventuit.adminstrativeapp.shared.validations.email.Email;
+import com.ventuit.adminstrativeapp.utils.RegexUtils;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,21 +20,29 @@ import lombok.NoArgsConstructor;
 public class CreateKeycloakUser {
 
     @NotBlank(message = "username cannot be empty")
+    @Size(min = 3, max = 255, message = "username must have a length between 3 and 255")
+    @Pattern(regexp = RegexUtils.USERNAME_PATTERN, message = "username contains invalid character")
     @UniqueUsername()
     private String username;
 
     @NotBlank(message = "email cannot be empty")
+    @Size(max = 255, message = "email cannot exceed 255 characters")
     @Email
     @UniqueEmail()
     private String email;
 
     @NotBlank(message = "first name cannot be empty")
+    @Size(max = 255, message = "first name cannot exceed 255 characters")
+    @Pattern(regexp = RegexUtils.NAME_PATTERN, message = "first name contains invalid character")
     private String firstName;
 
     @NotBlank(message = "last name cannot be empty")
+    @Size(max = 255, message = "last name cannot exceed 255 characters")
+    @Pattern(regexp = RegexUtils.NAME_PATTERN, message = "last name contains invalid character")
     private String lastName;
 
     @NotBlank(message = "password cannot be empty")
+    @Size(min = 8, max = 255, message = "password must have a length between 8 and 255")
     private String password;
 
 }
